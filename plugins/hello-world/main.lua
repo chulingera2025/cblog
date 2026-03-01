0c31eb4fcb6d683032db713a605c602fb6125848
chulingera2025 <chulingera2025@gmail.com>
2026-03-01 10:45:27 +0800

    fix: 修复 hello-world 示例插件使用 filter 注册 action hook 的错误
    将错误的 plugin.filter("after_render", ...) 改为 plugin.action，
    展示 ctx.posts 和 ctx.output_dir 上下文数据的用法。

--- plugins/hello-world/main.lua
+++ plugins/hello-world/main.lua
@@ -1,11 +1,10 @@
 -- 注册一个构建开始的 action hook
 plugin.action("after_load", 10, function(ctx)
     cblog.log.info("Hello from hello-world plugin!")
+    cblog.log.info("共加载 " .. #ctx.posts .. " 篇文章")
 end)
 
--- 注册一个简单的 filter hook 示例
-plugin.filter("after_render", 10, function(page)
-    -- 不修改内容，只记录日志
-    cblog.log.debug("hello-world: page rendered")
-    return page
+-- 注册一个 after_render action hook 示例
+plugin.action("after_render", 10, function(ctx)
+    cblog.log.debug("hello-world: 输出目录 = " .. (ctx.output_dir or "unknown"))
 end)
